0995f68758e41f29b8cc6bb4eb5fbea1e7059fc1
dblock <dblock@amazon.com>
2021-08-04 12:00:39 -0400

    Added Geospatial.
    Signed-off-by: dblock <dblock@amazon.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,5 +2,6 @@
 
 for repo in `gh repo list -L 100 --public opensearch-project | cut -f1 | cut -d/ -f2 | sort -f` 
 do
+    echo ⌛ Checking $repo ...
     meta project import $repo git@github.com:opensearch-project/$repo.git
 done
